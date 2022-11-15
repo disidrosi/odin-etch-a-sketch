51bc334deb34c6ce653b5663a1f9e0d0f12d7c67
disidrosi <cavalli.tobia@gmail.com>
2022-11-15 17:01:55 +0100

    Add grid resizing
    - Add createGrid() function to generate grid based on specific size.
    - Add resetGrid() function to reset all changes to current grid.
    - Add event listeners to buttons for changing grid to corresponding
      size.

--- script.js
+++ script.js
@@ -3,34 +3,61 @@
 const container = document.getElementById("container");
 let gridSize = 32;
 
+const btnGrid16 = document.getElementById("select-16");
+const btnGrid32 = document.getElementById("select-32");
+const btnGrid64 = document.getElementById("select-64");
+
+// Reset grid and all changes
+function resetGrid() {
+	container.innerText = "";
+	createGrid();
+}
+
 // Get current color choice
 function getColor() {
-	const colorChoice = "#c92a2a";
+	const colorChoice = "#4dabf7";
 	return colorChoice;
 }
 
 // Change box color
 function colorSwitch() {
-	const clrChoice = getColor();
+	const colorChoice = getColor();
 	if (!this.style.backgroundColor) {
-		this.style.setProperty("background-color", clrChoice);
+		this.style.setProperty("background-color", colorChoice);
 	}
 }
 
 // Create grid
-// Generate the rows
-for (let i = 1; i <= gridSize; i++) {
-	const row = document.createElement("div");
-	// Add boxes to each row
-	for (let j = 1; j <= gridSize; j++) {
-		const box = document.createElement("div");
-		// Change grid resolution without changing size
-		box.style.cssText = `width:${500 / gridSize / 16}rem; height:${
-			500 / gridSize / 16
-		}rem`;
-		// Color each box with a given color when the mouse passes over
-		box.addEventListener("mouseover", colorSwitch);
-		row.appendChild(box);
+function createGrid() {
+	// Generate the rows
+	for (let i = 1; i <= gridSize; i++) {
+		const row = document.createElement("div");
+		// Add boxes to each row
+		for (let j = 1; j <= gridSize; j++) {
+			const box = document.createElement("div");
+			// Change grid resolution without changing size
+			box.style.cssText = `width:${500 / gridSize / 16}rem; height:${
+				500 / gridSize / 16
+			}rem`;
+			// Color each box with a given color when the mouse passes over
+			box.addEventListener("mouseover", colorSwitch);
+			row.appendChild(box);
+		}
+		container.appendChild(row);
 	}
-	container.appendChild(row);
 }
+
+createGrid();
+
+btnGrid16.addEventListener("click", (e) => {
+	gridSize = 16;
+	resetGrid();
+});
+btnGrid32.addEventListener("click", (e) => {
+	gridSize = 32;
+	resetGrid();
+});
+btnGrid64.addEventListener("click", (e) => {
+	gridSize = 64;
+	resetGrid();
+});
